018585aa5aab3c5946a6020fa85cc7d9caade142
Yufan CHEN <108385820+CYF2000127@users.noreply.github.com>
2024-08-13 21:56:18 +0800

    Update train.sh

--- exps/train.sh
+++ exps/train.sh
@@ -3,14 +3,13 @@
 NUM_NODES=1
 NUM_GPUS_PER_NODE=2
 NODE_RANK=0
-
 BATCH_SIZE=64
 ACCUM_STEP=1
-
 MASTER_PORT=$(shuf -n 1 -i 10000-65535)
-
 DATESTR=$(date +"%m-%d-%H-%M")
+
 SAVE_PATH=/
+
 mkdir -p ${SAVE_PATH}
 
 set -x
@@ -19,9 +18,9 @@ torchrun \
     --nproc_per_node=$NUM_GPUS_PER_NODE --nnodes=$NUM_NODES --node_rank $NODE_RANK --master_addr localhost --master_port $MASTER_PORT \
     main.py \
     --data_path data \
-    --train_file pubchem/train_1m.csv \
-    --aux_file uspto_mol/train_680k.csv --coords_file aux_file \
-    --valid_file real/USPTO.csv \
+    --train_file pubchem/train_pubchem.csv \
+    --aux_file uspto/train_uspto.csv --coords_file aux_file \
+    --valid_file real/acs.csv \
     --test_file real/your_data_path \
     --vocab_file MolNexTR/vocab/vocab_chars.json \
     --formats chartok_coords,edges \
@@ -29,8 +28,6 @@ torchrun \
     --include_condensed \
     --coord_bins 64 --sep_xy \
     --input_size 384 \
-    --encoder convnext \
-    --decoder transformer \
     --encoder_lr 4e-4 \
     --decoder_lr 4e-4 \
     --save_path $SAVE_PATH --save_mode all \
