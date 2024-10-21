7bb28d528f101bbeb8443d8857b380caebd7eb29
Yufan CHEN <108385820+CYF2000127@users.noreply.github.com>
2024-10-21 16:29:57 +0800

    Add files via upload

---
+++ evaluate.py
@@ -0,0 +1,218 @@
+import json
+import argparse
+import numpy as np
+import multiprocessing
+import pandas as pd
+
+import rdkit
+from rdkit import Chem, DataStructs
+
+rdkit.RDLogger.DisableLog('rdApp.*')
+from SmilesPE.pretokenizer import atomwise_tokenizer
+
+
+def get_args():
+    """
+    Parse and return command-line arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gold_file', type=str, required=True)
+    parser.add_argument('--pred_file', type=str, required=True)
+    parser.add_argument('--pred_field', type=str, default='SMILES')
+    parser.add_argument('--num_workers', type=int, default=16)
+    parser.add_argument('--tanimoto', action='store_true')
+    parser.add_argument('--keep_main', action='store_true')
+    args = parser.parse_args()
+    return args
+
+
+def canonicalize_smiles(smiles, ignore_chiral=False, ignore_cistrans=False, replace_rgroup=True):
+    """
+    Canonicalize a SMILES string with options to ignore chirality, cis/trans stereochemistry, and replace R groups.
+
+    Args:
+        smiles (str): SMILES string to canonicalize.
+        ignore_chiral (bool): If True, ignore chirality during canonicalization.
+        ignore_cistrans (bool): If True, ignore cis/trans stereochemistry.
+        replace_rgroup (bool): If True, replace R groups with wildcard characters.
+
+    Returns:
+        str: Canonicalized SMILES string.
+        bool: True if successful, False otherwise.
+    """
+    if type(smiles) is not str or smiles == '':
+        return '', False
+    if ignore_cistrans:
+        smiles = smiles.replace('/', '').replace('\\', '')
+    if replace_rgroup:
+        tokens = atomwise_tokenizer(smiles)
+        for j, token in enumerate(tokens):
+            if token[0] == '[' and token[-1] == ']':
+                symbol = token[1:-1]
+                if symbol[0] == 'R' and symbol[1:].isdigit():
+                    tokens[j] = f'[{symbol[1:]}*]'
+                elif Chem.AtomFromSmiles(token) is None:
+                    tokens[j] = '*'
+        smiles = ''.join(tokens)
+    try:
+        canon_smiles = Chem.CanonSmiles(smiles, useChiral=(not ignore_chiral))
+        success = True
+    except:
+        canon_smiles = smiles
+        success = False
+    return canon_smiles, success
+
+
+def convert_smiles_to_canonsmiles(
+        smiles_list, ignore_chiral=False, ignore_cistrans=False, replace_rgroup=True, num_workers=16):
+    """
+    Convert a list of SMILES strings to canonical SMILES using parallel processing.
+
+    Args:
+        smiles_list (list): List of SMILES strings.
+        ignore_chiral (bool): If True, ignore chirality during canonicalization.
+        ignore_cistrans (bool): If True, ignore cis/trans stereochemistry.
+        replace_rgroup (bool): If True, replace R groups with wildcard characters.
+        num_workers (int): Number of parallel workers.
+
+    Returns:
+        list: List of canonical SMILES strings.
+        float: Average success rate of the conversion.
+    """
+    with multiprocessing.Pool(num_workers) as p:
+        results = p.starmap(canonicalize_smiles,
+                            [(smiles, ignore_chiral, ignore_cistrans, replace_rgroup) for smiles in smiles_list],
+                            chunksize=128)
+    canon_smiles, success = zip(*results)
+    return list(canon_smiles), np.mean(success)
+
+
+def _keep_main_molecule(smiles, debug=False):
+    try:
+        mol = Chem.MolFromSmiles(smiles)
+        frags = Chem.GetMolFrags(mol, asMols=True)
+        if len(frags) > 1:
+            num_atoms = [m.GetNumAtoms() for m in frags]
+            main_mol = frags[np.argmax(num_atoms)]
+            smiles = Chem.MolToSmiles(main_mol)
+    except Exception as e:
+        pass
+    return smiles
+
+
+def keep_main_molecule(smiles, num_workers=16):
+    """
+    Retain only the main (largest) molecule fragment from a SMILES string.
+
+    Args:
+        smiles (str): SMILES string.
+
+    Returns:
+        str: SMILES string for the main fragment.
+    """
+    with multiprocessing.Pool(num_workers) as p:
+        results = p.map(_keep_main_molecule, smiles, chunksize=128)
+    return results
+
+
+def tanimoto_similarity(smiles1, smiles2):
+    """
+    Compute the Tanimoto similarity between two SMILES strings.
+
+    Args:
+        smiles1 (str): First SMILES string.
+        smiles2 (str): Second SMILES string.
+
+    Returns:
+        float: Tanimoto similarity score.
+    """
+    try:
+        mol1 = Chem.MolFromSmiles(smiles1)
+        mol2 = Chem.MolFromSmiles(smiles2)
+        fp1 = Chem.RDKFingerprint(mol1)
+        fp2 = Chem.RDKFingerprint(mol2)
+        tanimoto = DataStructs.FingerprintSimilarity(fp1, fp2)
+        return tanimoto
+    except:
+        return 0
+
+
+def compute_tanimoto_similarities(gold_smiles, pred_smiles, num_workers=16):
+    """
+    Calculate Tanimoto similarities for lists of gold and predicted SMILES strings using parallel processing.
+
+    Args:
+        gold_smiles (list): List of gold SMILES strings.
+        pred_smiles (list): List of predicted SMILES strings.
+        num_workers (int): Number of parallel workers.
+
+    Returns:
+        list: List of Tanimoto similarity scores.
+    """
+    with multiprocessing.Pool(num_workers) as p:
+        similarities = p.starmap(tanimoto_similarity, [(gs, ps) for gs, ps in zip(gold_smiles, pred_smiles)])
+    return similarities
+
+
+class SmilesEvaluator(object):
+    def __init__(self, gold_smiles, num_workers=16, tanimoto=False):
+        self.gold_smiles = gold_smiles
+        self.num_workers = num_workers
+        self.tanimoto = tanimoto
+        self.gold_smiles_cistrans, _ = convert_smiles_to_canonsmiles(gold_smiles,
+                                                                     ignore_cistrans=True,
+                                                                     num_workers=num_workers)
+        self.gold_smiles_chiral, _ = convert_smiles_to_canonsmiles(gold_smiles,
+                                                                   ignore_chiral=True, ignore_cistrans=True,
+                                                                   num_workers=num_workers)
+        self.gold_smiles_cistrans = self._replace_empty(self.gold_smiles_cistrans)
+        self.gold_smiles_chiral = self._replace_empty(self.gold_smiles_chiral)
+
+    def _replace_empty(self, smiles_list):
+        """Replace empty SMILES in the gold, otherwise it will be considered correct if both pred and gold is empty."""
+        return [smiles if smiles is not None and type(smiles) is str and smiles != "" else "<empty>"
+                for smiles in smiles_list]
+
+    def evaluate(self, pred_smiles, include_details=False):
+        results = {}
+        if self.tanimoto:
+            results['tanimoto'] = np.mean(compute_tanimoto_similarities(self.gold_smiles, pred_smiles))
+        # Ignore double bond cis/trans
+        pred_smiles_cistrans, _ = convert_smiles_to_canonsmiles(pred_smiles,
+                                                                ignore_cistrans=True,
+                                                                num_workers=self.num_workers)
+        results['canon_smiles'] = np.mean(np.array(self.gold_smiles_cistrans) == np.array(pred_smiles_cistrans))
+        if include_details:
+            results['canon_smiles_details'] = (np.array(self.gold_smiles_cistrans) == np.array(pred_smiles_cistrans))
+        # Ignore chirality (Graph exact match)
+        pred_smiles_chiral, _ = convert_smiles_to_canonsmiles(pred_smiles,
+                                                              ignore_chiral=True, ignore_cistrans=True,
+                                                              num_workers=self.num_workers)
+        results['graph'] = np.mean(np.array(self.gold_smiles_chiral) == np.array(pred_smiles_chiral))
+        # Evaluate on molecules with chiral centers
+        chiral = np.array([[g, p] for g, p in zip(self.gold_smiles_cistrans, pred_smiles_cistrans) if '@' in g])
+        results['chiral'] = np.mean(chiral[:, 0] == chiral[:, 1]) if len(chiral) > 0 else -1
+        return results
+
+
+if __name__ == "__main__":
+    args = get_args()
+    gold_df = pd.read_csv(args.gold_file)
+    pred_df = pd.read_csv(args.pred_file)
+
+    if len(pred_df) != len(gold_df):
+        print(f"Pred ({len(pred_df)}) and Gold ({len(gold_df)}) have different lengths!")
+
+    # Re-order pred_df to have the same order with gold_df
+    image2goldidx = {image_id: idx for idx, image_id in enumerate(gold_df['image_id'])}
+    image2predidx = {image_id: idx for idx, image_id in enumerate(pred_df['image_id'])}
+    for image_id in gold_df['image_id']:
+        # If image_id doesn't exist in pred_df, add an empty prediction.
+        if image_id not in image2predidx:
+            pred_df = pred_df.append({'image_id': image_id, args.pred_field: ""}, ignore_index=True)
+    image2predidx = {image_id: idx for idx, image_id in enumerate(pred_df['image_id'])}
+    pred_df = pred_df.reindex([image2predidx[image_id] for image_id in gold_df['image_id']])
+
+    evaluator = SmilesEvaluator(gold_df['SMILES'], args.num_workers, args.tanimoto)
+    scores = evaluator.evaluate(pred_df[args.pred_field])
+    print(json.dumps(scores, indent=4))
